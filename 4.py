a398479a3446af898d2f2e0cf756bec68c26b11a
17ashishjose <165980854+17ashishjose@users.noreply.github.com>
2024-05-15 11:26:24 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,10 @@
+f=int(input("ENTER THE FIRST NO"))
+s=int(input("ENTER THE SECOND NO"))
+a=f+s
+print("ADDITION",a)
+b=f-s
+print("SUBSTRATION",b)
+c=f*s
+print("MULTIPLICATION",c)
+d=f/s
+print("DIVISION",d)
